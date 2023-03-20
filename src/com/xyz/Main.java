aff48f57fc4a7d93dac786978148d73a75d4963c
Sneha_Matte <sneharmatte@gmail.com>
2023-03-20 14:44:18 +0530

    fixed main class and add comment

--- src/com/xyz/Main.java
+++ src/com/xyz/Main.java
@@ -15,6 +15,7 @@ System.out.println("Enter the first number:");
 a =sc.nextInt();
 System.out.println("Enter the second number:");
 b=sc.nextInt();
+System.out.println("Calculation of the number");
 System.out.println("The sum of the first and second no : "+object.sum(a,b));
 System.out.println("The sub of first and second no : "+object.sub(a,b));
 System.out.println("The mul of first and second no: "+object.mul(a,b));
